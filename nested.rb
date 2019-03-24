d3981c8152a5bd0b29ce6261c4c2bc33319c9006
Mario Adamo Jr <marioadamojr@gmail.com>
2019-03-24 02:54:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ programmer_hash =
         :languages => ["C"]
       }
    }
-programmer_hash[:yukihiro_matsumoto] =
+programmer_hash[:yukihiro_matsumoto] <<
 {
   :known_for => "Ruby",
   :languages => ["LISP", "C"]
